0fe6851f078b3ffbe8be1e9f6a38927d92de9652
kiyani <bamba.kiyani@gmail.com>
2020-03-26 23:49:36 -0400

    added file name and path to ls8

--- ls8.py
+++ ls8.py
@@ -6,7 +6,13 @@ from cpu import CPU
 
 if len(sys.argv) != 2:
     print("ERR: No file given:\t python ls8.py examples/file_name")
-    
+    sys.exit(1)
+
+script_dir = os.path.dirname(__file__)
+file_name = sys.argv[1] 
+
+file_path = os.path.join(script_dir, file_name)
+
 cpu = CPU()
 cpu.load(file_name)
 cpu.run()
